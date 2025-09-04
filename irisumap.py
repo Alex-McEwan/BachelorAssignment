12de5f143399c0d6341ff6e4e017628571248dac
Alex McEwan <a.mcewan@student.utwente.nl>
2025-09-04 17:26:10 +0200

    Initial commit

---
+++ irisumap.py
@@ -0,0 +1,52 @@
+
+from sklearn.datasets import load_iris
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import umap 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+
+iris = load_iris(as_frame=True)
+df = iris.frame
+print(df.head())
+
+df = df.dropna()
+
+X = df.drop(columns=["target"])
+df["target"]
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+
+y = df["target"].values
+print(X_scaled.shape)
+
+
+reducer = umap.UMAP()
+X_umap = reducer.fit_transform(X_scaled)
+
+print(X_umap.shape)
+
+
+species_names = iris.target_names
+
+palette = sns.color_palette("tab10", len(species_names))
+
+for i, name in enumerate(species_names):
+    plt.scatter(
+        X_umap[y == i, 0],
+        X_umap[y == i, 1],
+        label=name,
+        s=5,
+        c=[palette[i]]
+    )
+
+plt.legend(title="Species", bbox_to_anchor=(1.05, 1), loc='upper left')
+
+plt.xlabel("UMAP-1")
+plt.ylabel("UMAP-2")
+plt.title("UMAP projection of Iris dataset")
+plt.savefig("plots/iris_umap.png", dpi=300, bbox_inches="tight")
