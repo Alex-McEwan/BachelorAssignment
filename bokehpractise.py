dac2c73e69e501365d32d483877b9451a7c41d4b
Alex McEwan <a.mcewan@student.utwente.nl>
2025-09-08 11:58:53 +0200

    Create a folder for the bokeh plots and save the html there

--- bokehpractise.py
+++ bokehpractise.py
@@ -2,6 +2,8 @@ import pandas as pd
 from bokeh.plotting import figure, show, output_file
 from bokeh.models import ColumnDataSource
 
+output_folder = "bokehfiles/"
+
 df = pd.DataFrame({
     "x": list(range(0, 11)), 
     "y": list(range(0, 11))    
@@ -13,5 +15,6 @@ p = figure(title="y = x line", x_axis_label="x", y_axis_label="y")
 
 p.line("x", "y", source=source, line_width=2)
 
-output_file("bokehpractise.html")
+output_file(output_folder + "bokehplot.html")
+
 show(p)
