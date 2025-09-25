5e0ef67a8261c1b104dc9883daac168347ba146d
Alex McEwan <a.mcewan@student.utwente.nl>
2025-09-25 12:49:03 +0200

    Create some more umaps with different metrics and different neighbour values

--- bokeh_implementations/lso_dos_data/umap_data.py
+++ bokeh_implementations/lso_dos_data/umap_data.py
@@ -29,8 +29,8 @@ energy_columns = [col for col in df.columns if col != "material"]
 
 X_sparse = sparse.csr_matrix(df[energy_columns].values)
 
-N_NEIGHBORS = 15
-DISTANCE_METRIC = "cosine"
+N_NEIGHBORS = 20
+DISTANCE_METRIC = "euclidean"
 
 scaler = MaxAbsScaler()
 X_scaled = scaler.fit_transform(X_sparse)
